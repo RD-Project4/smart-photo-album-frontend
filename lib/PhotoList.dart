5d19e810730a4136a1517764baa71ca57ba03e6b
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2021-11-03 02:21:35 +0800

    bottom navigation & incomplete grouped grid view

--- lib/PhotoList.dart
+++ lib/PhotoList.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:grouped_list/grouped_list.dart';
+import 'package:smart_album/widgets/GroupedView.dart';
 
 List _elements = [
-  {'name': 'John', 'group': 'Team A'},
-  {'name': 'Will', 'group': 'Team B'},
-  {'name': 'Beth', 'group': 'Team A'},
-  {'name': 'Miranda', 'group': 'Team B'},
-  {'name': 'Mike', 'group': 'Team C'},
-  {'name': 'Danny', 'group': 'Team C'},
+  {'name': '1.jpg', 'time': 'Oct.12'},
+  {'name': '2.jpg', 'time': 'Oct.10'},
+  {'name': '3.jpg', 'time': 'Oct.10'},
+  {'name': '4.jpg', 'time': 'Oct.10'},
 ];
 
 class PhotoList extends StatelessWidget {
@@ -17,37 +16,42 @@ class PhotoList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GroupedListView<dynamic, String>(
-      padding: isHasTopBar ? const EdgeInsets.only(top: kToolbarHeight) : null,
-      elements: _elements,
-      groupBy: (element) => element['group'],
-      groupComparator: (value1, value2) => value2.compareTo(value1),
-      itemComparator: (item1, item2) => item1['name'].compareTo(item2['name']),
-      order: GroupedListOrder.DESC,
-      floatingHeader: false,
-      groupSeparatorBuilder: (String value) => Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          value,
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-      ),
-      itemBuilder: (c, element) {
-        return Card(
-          elevation: 8.0,
-          margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-          child: Container(
-            child: ListTile(
-              contentPadding:
-                  EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-              leading: Icon(Icons.account_circle),
-              title: Text(element['name']),
-              trailing: Icon(Icons.arrow_forward),
+    return GroupedView<dynamic, String>(
+        padding:
+            isHasTopBar ? const EdgeInsets.only(top: kToolbarHeight) : null,
+        elements: _elements,
+        groupBy: (element) => element['time'],
+        groupComparator: (value1, value2) => -value2.compareTo(value1),
+        order: GroupedListOrder.DESC,
+        floatingHeader: false,
+        groupSeparatorBuilder: (String value) => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                value,
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-        );
-      },
-    );
+        sectionBuilder: (context, elementList) {
+          return GridView.count(
+              // Create a grid with 2 columns. If you change the scrollDirection to
+              // horizontal, this produces 2 rows.
+              crossAxisCount: 2,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              // Generate 100 widgets that display their index in the List.
+              children: elementList
+                  .map((element) => Container(
+                        height: 120.0,
+                        width: 120.0,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("images/" + element['name']),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ))
+                  .toList());
+        });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,48 @@ import 'package:flutter/material.dart';
 import 'package:smart_album/PhotoList.dart';
 import 'package:smart_album/SearchBar.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: SearchBar(scrollTarget: PhotoList(isHasTopBar: true))),
+        resizeToAvoidBottomInset: false,
+        body: SearchBar(scrollTarget: PhotoList(isHasTopBar: true)),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category_outlined),
+              label: 'Category',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.backup_outlined),
+              label: 'Backup',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.indigo,
+          onTap: _onItemTapped,
+        ),
+      ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
