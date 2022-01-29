4e2b74c89b01fa82a1ca710ef32ab9be2bcb80e7
chan_we <chan_we@foxmail.com>
2022-01-29 13:55:35 +0800

    feat: step

--- lib/RegisterPage.dart
+++ lib/RegisterPage.dart
@@ -1,23 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:smart_album/widgets/HiddenAppbar.dart';
 
-class RegisterPage extends StatefulWidget{
+class RegisterPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: HiddenAppbar(),
-      body: Center(
-        child: Stepper(type: StepperType.horizontal, steps: <Step>[
-          Step(title: Text(''), content: Container()),
-          Step(title: Text(''), content: Container()),
-          Step(title: Text(''), content: Container())
-        ]),
-      ),
-    );
+        appBar: HiddenAppbar(),
+        body: Center(
+            child: Stepper(
+                currentStep: _index,
+                type: StepperType.horizontal,
+                onStepCancel: () {
+                  if (_index > 0) {
+                    setState(() {
+                      _index -= 1;
+                    });
+                  }
+                },
+                onStepContinue: () {
+                  if (_index < 2) {
+                    setState(() {
+                      _index += 1;
+                    });
+                  }
+                },
+                steps: <Step>[
+              Step(
+                  title: Text(''),
+                  isActive: _index >= 0,
+                  content: Container(
+                      child: TextField(
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter your email'),
+                  ))),
+              Step(
+                  title: Text(''),
+                  isActive: _index >= 1,
+                  content: Container(
+                      child: TextField(
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter your code'),
+                  ))),
+              Step(
+                  title: Text(''),
+                  isActive: _index >= 2,
+                  content: Container(
+                      child: TextField(
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter your name'),
+                  )))
+            ])));
   }
 }
