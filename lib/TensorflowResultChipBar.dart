d54a6b1eee26b20deb2b928bcd1182ee4eaefa91
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2022-01-30 01:36:11 +0800

    多线程

--- lib/TensorflowResultChipBar.dart
+++ lib/TensorflowResultChipBar.dart
@@ -1,37 +1,37 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:smart_album/tensorflow/TensorflowProvider.dart';
 import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 
 import 'common/Global.dart';
+import 'widgets/ColorfulProgressBar.dart';
 
 class ChipBar<T extends dynamic> extends StatelessWidget {
   final T element;
 
-  ChipBar({Key? key, required this.element}) : super(key: key);
+  const ChipBar({Key? key, required this.element}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Category>>(
-      future: TensorflowProvider.get().then((provider) => provider.recognize(
-          File(Global.ROOT_PATH + element.relativePath + element.title))),
-      initialData: List.empty(),
+      future: TensorflowProvider.recognizeFile(
+          Global.ROOT_PATH + element.relativePath + element.title),
+      initialData: null,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        return Container(
-            height: 60,
-            child: snapshot.data != null
-                ? ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: (snapshot.data as List<Category>)
-                        .where((element) => element.score > 0.5)
-                        .take(5)
-                        .map((element) => Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 8),
-                            child: Chip(label: Text(element.label))))
-                        .toList(),
-                  )
-                : Chip(label: Text("Loading...")));
+        return (snapshot.connectionState == ConnectionState.done &&
+                snapshot.data != null)
+            ? Container(
+                height: 60,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: (snapshot.data as List<Category>)
+                      .where((element) => element.score > 0.5)
+                      .take(5)
+                      .map((element) => Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8),
+                          child: Chip(label: Text(element.label))))
+                      .toList(),
+                ))
+            : ColorfulProgressBar();
       },
     );
   }
--- lib/tensorflow/TensorflowProvider.dart
+++ lib/tensorflow/TensorflowProvider.dart
@@ -1,46 +1,57 @@
 import 'dart:io';
+import 'dart:isolate';
 import 'dart:math';
 
+import 'package:flutter/services.dart';
+import 'package:smart_album/util/ServiceUtil.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 
 class TensorflowProvider {
-  // Singleton
-  TensorflowProvider._internal();
-
-  static Future<TensorflowProvider> create() async {
-    TensorflowProvider provider = TensorflowProvider._internal();
-    await provider.loadModel();
-    await provider.loadLabels();
-    return provider;
-  }
-
-  static Future<TensorflowProvider> get() async {
-    if (_instance == null) {
-      _instance = await TensorflowProvider.create();
-    }
-    return _instance!;
-  }
-
-  static TensorflowProvider? _instance;
+  static const String SERVICE_NAME = "TensorflowService";
 
   late List<String> _labels;
   late Interpreter _interpreter;
   late List<int> _outputShape;
   late TfLiteType _outputType;
 
-  Future<void> loadModel() async {
-    _interpreter = await Interpreter.fromAsset("mobilenet.tflite");
+  TensorflowProvider(ByteData model, String labels) {
+    _loadModelFromByteData(model);
+    _loadLabels(labels);
+  }
+
+  void _loadModelFromByteData(ByteData model) {
+    _interpreter = Interpreter.fromBuffer(model.buffer.asUint8List(),
+        options: InterpreterOptions()..useNnApiForAndroid = true);
     _outputShape = _interpreter.getOutputTensor(0).shape;
     _outputType = _interpreter.getOutputTensor(0).type;
   }
 
-  Future<void> loadLabels() async {
-    _labels = await FileUtil.loadLabels("assets/labels.txt");
+  void _loadLabels(String labels) {
+    _labels = FileUtil.labelListFromString(labels);
   }
 
-  List<Category> recognize(File imageFile) {
-    TensorImage tensorImage = TensorImage.fromFile(imageFile);
+  static Future<List<Category>> recognizeFile(String path) async {
+    Service service = await ServiceUtils.getService(
+        SERVICE_NAME, TensorflowProvider._recognizeFileInService, {
+      "model": await rootBundle.load("assets/model.tflite"),
+      "labels": await rootBundle.loadString("assets/labels.txt")
+    });
+    return await service.execute(path);
+  }
+
+  static Future<void> _recognizeFileInService(
+      SendPort sendPort, ReceivePort receivePort, Map? initArgs) async {
+    if (initArgs == null) throw Exception("Can not init provider");
+    TensorflowProvider? provider =
+        TensorflowProvider(initArgs["model"], initArgs["labels"]);
+    await for (final msg in receivePort) {
+      sendPort.send(provider._recognizeFile(msg));
+    }
+  }
+
+  List<Category> _recognizeFile(String path) {
+    TensorImage tensorImage = TensorImage.fromFile(File(path));
     int cropSize = min(tensorImage.height, tensorImage.width);
     ImageProcessor imageProcessor = ImageProcessorBuilder()
         .add(ResizeWithCropOrPadOp(cropSize, cropSize))
