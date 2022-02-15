394cc67ced3fdafa5c6b33d143098da8a0cf4ad8
chan_we <chan_we@foxmail.com>
2022-02-15 13:20:03 +0800

    fix: 提取代码

--- lib/widgets/PhotoToolBar.dart
+++ lib/widgets/PhotoToolBar.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 import 'package:smart_album/PhotoEditPage.dart';
 import 'package:smart_album/bloc/photo_list/PhotoListCubit.dart';
 import 'package:flutter/services.dart';
+import 'package:smart_album/util/ShareUtil.dart';
 
 class PhotoToolBar extends StatelessWidget {
   final photoIndex;
@@ -25,7 +24,7 @@ class PhotoToolBar extends StatelessWidget {
               text: "Share",
               onTap: () {
                 // Fluttertoast.showToast(msg: "Share");
-                _openShareBottomSheet(context, 1);
+                ShareUtil.openShareBottomSheet(context, 1);
                 // _shareToEveryone(context);
               }),
           IconText(
@@ -47,110 +46,7 @@ class PhotoToolBar extends StatelessWidget {
     );
   }
 
-  Future _openShareBottomSheet(BuildContext context, int photoNum) async {
-    final option = await showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context) {
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ListTile(
-                  title: RichText(
-                text: TextSpan(
-                  text: 'Share ',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.black),
-                  children: <TextSpan>[
-                    TextSpan(
-                        text: '$photoNum',
-                        style: TextStyle(color: Colors.blueAccent)),
-                    TextSpan(text: ' photo${photoNum > 1 ? 's' : ''} with:'),
-                  ],
-                ),
-              )),
-              ListTile(
-                title: Text('Everyone', textAlign: TextAlign.center),
-                onTap: () {
-                  Navigator.pop(context, '分享给所有人');
-                  _shareToEveryone(context);
-                },
-              ),
-              ListTile(
-                title: Text('Only my friends', textAlign: TextAlign.center),
-                onTap: () {
-                  Navigator.pop(context, '只分享给朋友');
-                },
-              ),
-              ListTile(
-                title: Text(
-                  'Cancel',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.red),
-                ),
-                onTap: () {
-                  Navigator.pop(context, '取消');
-                },
-              ),
-            ],
-          );
-        });
 
-    print(option);
-  }
-
-  Future _shareToEveryone(BuildContext context) async {
-    // TODO: 从api获取分享链接并赋值给shareUrl
-    var shareUrl = 'https://github.com/RD-Project4/smart-photo-album-frontend';
-
-    final option = await showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context) {
-          return Column(mainAxisSize: MainAxisSize.min, children: [
-            QrImage(
-              data: shareUrl,
-              version: QrVersions.auto,
-              size: 200.0,
-              embeddedImage: AssetImage('images/logo_white_bg.png'),
-              errorCorrectionLevel: QrErrorCorrectLevel.H,
-            ),
-            ListTile(
-              title: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // IconButton(
-                  //   icon: Icon(Icons.qr_code_2),
-                  //   onPressed: () {},
-                  // ),
-                  Expanded(
-                    child: Text(
-                      shareUrl,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  CopyBtn(
-                    text: shareUrl,
-                  )
-                ],
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Cancel',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.red),
-              ),
-              onTap: () {
-                Navigator.pop(context, '取消');
-              },
-            ),
-          ]);
-        });
-
-    print(option);
-  }
 }
 
 class IconText extends StatelessWidget {
